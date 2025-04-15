fe3996f8daf07775e4a16ab874d51bfedb33292d
huzaifa016 <167076172+huzaifa016@users.noreply.github.com>
2025-04-15 19:15:51 +0000

    datatypes

---
+++ basics_js/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all js code as newer version
+
+//alert (3 + 3)  //we are using nodejs , not browser
+
+console.log( 3 
+    +3)          // code readability should be high
+    
+ 
+let name = "Huzaifa"
+let age = 18
+let isLoggedIn = false 
+let state = null
+
+//========== premitive datatypes =======
+//numbers => 2^53 (range)
+//bigint
+//strings => ""
+//boolean => true/false
+//null => standalone value
+//undefined => undefined
+//symbole => unique
+
+
+//========== non premitive datatypes====
+//objects
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
