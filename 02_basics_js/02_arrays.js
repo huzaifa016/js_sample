913a156fe0e1ea01c9880e1ef28e461dd7fee956
huzaifa016 <167076172+huzaifa016@users.noreply.github.com>
2025-05-05 16:43:16 +0000

    Arrays

--- 02_basics_js/02_arrays.js
+++ 02_basics_js/02_arrays.js
@@ -20,9 +20,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Huzaifa"))
+console.log(Array.from("Huzaifa"))
+console.log(Array.from({name: "huzaifa"})) // interesting
 
 let score1 = 100
 let score2 = 200
