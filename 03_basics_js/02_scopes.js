a925e961436916b7717c532590f6427b513ef261
huzaifa016 <167076172+huzaifa016@users.noreply.github.com>
2025-05-13 07:12:23 +0000

    scopes

---
+++ 03_basics_js/02_scopes.js
@@ -0,0 +1,57 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "huzaifa"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Huzaifa"
+    if (username === "Huzaifa") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))    //gives o/p as 6
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)                       //gives  error
+const addTwo = function(num){  //expression holding func
+    return num + 2
+}
